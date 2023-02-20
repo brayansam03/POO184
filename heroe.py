c484398e20bcdcde731babe9b27e69652e964332
brayansam03 <125929734+brayansam03@users.noreply.github.com>
2023-02-20 17:18:32 -0600

    Creamos instancia Heroe y las usamos

---
+++ heroe.py
@@ -0,0 +1,9 @@
+from Personaje import * 
+
+#1. Crear objeto de clase Personaje
+heroe= Personaje()
+
+#2. Usar atributos
+print("El personaje se llama: "+ heroe.nombre)
+print("Pertenece a la especie: "+ heroe.especie)
+print("Y tiene una altura de: "+ heroe.altura)
