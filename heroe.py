db4bad4deefe93bda29ba8088a5836c4eed23a65
brayansam03 <125929734+brayansam03@users.noreply.github.com>
2023-02-22 17:12:42 -0600

    Agregamos Constructor

--- heroe.py
+++
@@ -1,9 +0,0 @@
-from Personaje import * 
-
-#1. Crear objeto de clase Personaje
-heroe= Personaje()
-
-#2. Usar atributos
-print("El personaje se llama: "+ heroe.nombre)
-print("Pertenece a la especie: "+ heroe.especie)
-print("Y tiene una altura de: "+ heroe.altura)
---
+++ objetos.py
@@ -0,0 +1,36 @@
+#solicitar datos     
+from Personaje import*
+print("")
+print("Datos heroe")
+espH=input("Escribe la especie del Héroe: ")
+nomH=input("Escribe el nombre del Héroe: ")
+altH=float(input("Escribe ela altura del Héroe: "))
+recargaH=int(input("¿Cuántas balas recargas al Héroe? "))
+
+print("")
+print("Datos villano")
+espV=input("Escribe la especie del Villano: ")
+nomV=input("Escribe el nombre del Villano: ")
+altV=float(input("Escribe ela altura del Villano: "))
+recargaV=int(input("¿Cuántas balas recargas al Villano? "))
+
+#Crear objeto de clase
+heroe= personaje(espH,nomH,altH)
+villano=personaje(espV,nomV,altV)
+
+#Atributos
+print("Objeto Heroe")
+print("El personaje se llama: " + heroe.nombre)
+print("El personaje es un: " + heroe.especie)
+print("El personaje mide: " + str(heroe.altura))
+heroe.correr(True)
+heroe.lanzar_granadas()
+heroe.recargar_arma(recargaH)
+
+print("Objeto Villano")
+print("El personaje se llama: " + villano.nombre)
+print("El personaje es un: " + villano.especie)
+print("El personaje mide: " + str(villano.altura))
+villano.correr(True)
+villano.lanzar_granadas()
+villano.recargar_arma(recargaV)
