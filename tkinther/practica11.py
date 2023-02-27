ca50ff8ab159ba75c2dae09ad7459d71efa4fece
brayansam03 <125929734+brayansam03@users.noreply.github.com>
2023-02-27 17:19:37 -0600

    TK, frames, buttons

---
+++ tkinther/practica11.py
@@ -0,0 +1,39 @@
+from tkinter import Tk, Button, Frame, messagebox
+
+def mostrarMensajes():
+    messagebox.showinfo("Aviso: ", "Presionaste el boton azul")
+
+#1.Instanciamos el objeto ventana
+ventana= Tk()
+ventana.title("Ejemplo de 3 Frames")
+ventana.geometry("600x400")
+
+#2. agregamos los Frames
+seccion1= Frame(ventana, bg="red")
+seccion1.pack(expand=True, fill='both')
+
+seccion2= Frame(ventana, bg="purple")
+seccion2.pack(expand=True, fill='both')
+
+seccion3= Frame(ventana, bg="black")
+seccion3.pack(expand=True, fill='both')
+
+#3. Agregamos botones 
+botonAzul= Button(seccion1, text="Boton azul", fg="blue", command=mostrarMensajes) 
+botonAzul.place(x="60", y=60)
+
+botonNegro= Button(seccion2, text="Boton negro", fg="white", bg="black")
+botonNegro.grid(row=0, column=0)
+
+botonAmarillo= Button(seccion2, text="Boton amarillo", bg="#ffff4d")
+botonAmarillo.grid(row=1, column=1)
+
+botonVerde= Button(seccion3, text="Boton verde", bg="#99e69")
+botonVerde.configure(height=2, width=10)
+botonVerde.pack()
+
+
+
+#llamamos al Main
+ventana.mainloop()
+
